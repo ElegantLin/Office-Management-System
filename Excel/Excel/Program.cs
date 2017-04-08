aa70a8b0b60934dc24196136e916dcd724267c25
ElegantLin <elegant.lin21@gmail.com>
2017-04-08 10:22:50 +0800

    V1.1

--- Excel/Excel/Program.cs
+++ Excel/Excel/Program.cs
@@ -11,8 +11,10 @@ using Word = Microsoft.Office.Interop.Word;
 using Microsoft.Office.Core;
 using System.Reflection;
 using System.Text.RegularExpressions;
-
-
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Collections;
 
 namespace excel1
 {
@@ -23,7 +25,8 @@ namespace excel1
         static void Main(string[] args)
         {
             Excel.Application excel = new Excel.Application();
-            string address = "C:\\Users\\Elegant\\Desktop\\1.xls";
+            string excel_address = "C:\\Users\\Elegant\\Desktop\\1.xls";
+            string word_address = "C:\\Users\\Elegant\\Desktop\\1.doc";
             string thanksEnding = "候会，收到烦复！党办小唐";
             string endingPlease = "请您会期关心时间情况通报的群消息，并提前在";
             string beginning = "尊敬的";
@@ -34,7 +37,7 @@ namespace excel1
             {
                 excel.Visible = true;
                 excel.UserControl = true; //Read-Only Mode;
-                Excel.Workbook workbook = excel.Application.Workbooks.Open(address, missing, true, missing, missing,
+                Excel.Workbook workbook = excel.Application.Workbooks.Open(excel_address, missing, true, missing, missing,
                     missing, missing, missing, missing, false, missing, missing, missing, missing, missing);
                 Excel.Worksheet worksheet = (Excel.Worksheet)workbook.Worksheets.get_Item(1);
 
@@ -69,42 +72,72 @@ namespace excel1
                     }
                 }
 
-                
+
 
                 //Get other elements
-                Excel.Range topics = worksheet.Cells.get_Range("B2", "B" + rowNum);
-                Excel.Range presenter = worksheet.Cells.get_Range("C2", "C" + rowNum);
-                Excel.Range school_leader = worksheet.Cells.get_Range("D2", "D" + rowNum);
-                Excel.Range others = worksheet.Cells.get_Range("E2", "E" + rowNum);
-                Excel.Range time = worksheet.Cells.get_Range("F2", "F" + rowNum);
+                //Excel.Range topics = worksheet.Cells.get_Range("B2", "B" + rowNum);
+                //Excel.Range presenter = worksheet.Cells.get_Range("C2", "C" + rowNum);
+                //Excel.Range school_leader = worksheet.Cells.get_Range("D2", "D" + rowNum);
+                //Excel.Range others = worksheet.Cells.get_Range("E2", "E" + rowNum);
+                //Excel.Range time = worksheet.Cells.get_Range("F2", "F" + rowNum);
+
 
                 //From Range to object
-                object[,] topic_o = (object[,])topics.Value2;
-                object[,] presenter_o = (object[,])presenter.Value2;
-                object[,] school_leader_o = (object[,])school_leader.Value2;
-                object[,] others_o = (object[,])others.Value2;
-                object[,] time_o = (object[,])time.Value2;
+                //object[,] topic_o = (object[,])topics.Value2;
+                //object[,] presenter_o = (object[,])presenter.Value2;
+                //object[,] school_leader_o = (object[,])school_leader.Value2;
+                //object[,] others_o = (object[,])others.Value2;
+                //object[,] time_o = (object[,])time.Value2;
 
-                //From Object to String
-                string[] topic_str = new string[rowNum - 1];
-                string[] presenter_str = new string[rowNum - 1];
-                string[] school_leader_str = new string[rowNum - 1];
-                string[] others_str = new string[rowNum - 1];
-                string[] time_str = new string[rowNum - 1];
+                Word.Application word = new Word.Application();
+                word.Visible = true;
+                Word.Document document = word.Documents.Open(word_address, missing, false, true, missing, missing, missing, missing
+                    , missing, missing, missing, true, true, missing, missing, missing);
+                document.PageSetup.PaperSize = Word.WdPaperSize.wdPaperA4;
+                
+                string[,] content = new string[rowNum - 2, colNum - 1];
+                
+                ArrayList al = new ArrayList();
+                string message = beginning; // 尊敬的
 
-                for (int i = 1;i<=rowNum-1;i++)
+                for (int i = 2; i <= rowNum - 1; i++)
                 {
-                    topic_str[i-1] = topic_o[i,1].ToString();
-                    presenter_str[i - 1] = presenter_o[i,1].ToString();
-                    school_leader_str[i - 1] = school_leader_o[i, 1].ToString();
-                    others_str[i - 1] = school_leader_o[i, 1].ToString();
-                    time_str[i - 1] = time_o[i, 1].ToString();
+                    string[] others = worksheet.Cells[i, 4].Value.ToString().Split(new char[2] { '、', '，' });
+                    string topic = worksheet.Cells[i, 1].Value.ToString();
+                    string presenter = worksheet.Cells[i, 2].Value.ToString();
+                    string leader = worksheet.Cells[i, 3].Value.ToString();
+                    string time = worksheet.Cells[i, 5].Value.ToString();
+
+                    foreach (string temp in others)
+                    {
+                        al.Add(temp);
+                    }
+                    //for (j = 1; j < colNum; j++)
+                    //{
+                    //    content[i - 2, j - 1] = worksheet.Cells[i, j].Value.ToString();
+                    //    if (j == 3)
+                    //    {
+                    //        string[] others = content[i - 2, j].Split(new char[2] { '、', '，' });
+                    //        foreach(string temp in others)
+                    //        {
+                    //            al.Add(temp);
+                    //        }
+                    //    }
+                    //}
+                    foreach (Array array in al)
+                    {
+                        //message = message + array.ToString() + '，' + zidingyu + content[] 
+                    }
+                   
+                   
                 }
 
-                for(int i = 0;i<others_str.Length;i++)
-                {
+                
 
-                }
+                //for(int i = 0;i<others_str.Length;i++)
+                //{
+
+                //}
 
             }
             catch (Exception ex)
